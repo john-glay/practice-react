a40f2a148868bbb2d31827c8c38a4e823242220a
john-glay <johnglayc.bunao@gmail.com>
2022-09-20 15:47:08 +0800

    Reverse String | Done

--- src/pages/Strings.jsx
+++ src/pages/Strings.jsx
@@ -1,6 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function Strings() {
+  const [input, setInput] = useState("");
+
+  const getReverseString = () => {
+    return input.split("").reverse("").join("").split(" ").reverse("").join(" ");
+
+    // split("") is to split the strings individually
+    // reverse("") is to reverse the string
+    // join("") is to join the strings back together
+    // (" ") is to give them space
+  };
+
   return (
     <div className="container p-5">
       <div className="row">
@@ -9,9 +20,13 @@ export default function Strings() {
             type="text"
             className="w-100 lead"
             placeholder="Input"
+            value={input}
+            onChange={(e) => {
+              setInput(e.target.value);
+            }}
           />
         </div>
-        <h1 className="col-12 w-100 text-primary">Hello World</h1>
+        <h1 className="col-12 w-100 text-primary">{getReverseString()}</h1>
       </div>
     </div>
   );
