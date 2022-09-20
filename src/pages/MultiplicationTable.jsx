86252ea9efdfd1b52a39caa989d6f88721772f51
john-glay <johnglayc.bunao@gmail.com>
2022-09-20 17:17:16 +0800

    Multiplication Table

--- src/pages/MultiplicationTable.jsx
+++ src/pages/MultiplicationTable.jsx
@@ -1,6 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function MultiplicationTable() {
+  const [input, setInput] = useState("");
+  const sequence = [];
+
+  const getMultiplicationTable = () => {
+    // Check if letter or null
+    if (isNaN(input) || !input) {
+      return "Please enter a Number";
+    }
+
+    // Create a multiplication table
+    else {
+      for (let i = 1; i <= 10; i++) {
+        // Push inside the array
+        sequence.push(i * input);
+      }
+
+      return sequence.map((data) => (
+        <h1 className="col-12 w-100 text-primary">{data}</h1>
+      ));
+    }
+  };
   return (
     <div className="container p-5">
       <div className="row">
@@ -9,10 +30,14 @@ export default function MultiplicationTable() {
             type="text"
             className="w-100 lead"
             placeholder="Input"
+            value={input}
+            onChange={(e) => {
+              setInput(e.target.value);
+            }}
           />
         </div>
         <h1 className="col-12 w-100 text-primary">
-          Hello World
+          {getMultiplicationTable()}
         </h1>
       </div>
     </div>
