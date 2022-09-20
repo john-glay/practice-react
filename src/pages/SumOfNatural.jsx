e9988529a8aa09e6f5c7defa5e4b903ba7809485
john-glay <johnglayc.bunao@gmail.com>
2022-09-20 17:24:03 +0800

    Sum of Natural Numbers

--- src/pages/SumOfNatural.jsx
+++ src/pages/SumOfNatural.jsx
@@ -1,6 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function SumOfNatural() {
+  const [input, setInput] = useState("");
+
+  const getSumOfNatural = () => {
+    // Check if letter or null
+    if (isNaN(input) || !input) {
+      return "Please enter a Number";
+    }
+
+    // Create sum of natural numbers
+    let total = 0;
+    for (let i = 1; i <= input; i++) {
+      total = total + i;
+    }
+
+    return total;
+  };
+  
   return (
     <div className="container p-5">
       <div className="row">
@@ -9,11 +26,13 @@ export default function SumOfNatural() {
             type="text"
             className="w-100 lead"
             placeholder="Input"
+            value={input}
+            onChange={(e) => {
+              setInput(e.target.value);
+            }}
           />
         </div>
-        <h1 className="col-12 w-100 text-primary">
-          Hello World
-        </h1>
+        <h1 className="col-12 w-100 text-primary">{getSumOfNatural()}</h1>
       </div>
     </div>
   );
