7e6558542651ce4abc3eb9c274b223482de4ff6b
john-glay <johnglayc.bunao@gmail.com>
2022-09-21 14:26:42 +0800

    Number Right Triangle

--- src/pages/NumberRightTriangle.jsx
+++ src/pages/NumberRightTriangle.jsx
@@ -1,6 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function NumberRightTriangle() {
+  const [input, setInput] = useState("");
+
+  const getNumberRightTriangle = () => {
+    // Check if letter or null
+    if (isNaN(input) || !input) {
+      return (
+        <h1 className="col-12 w-100 text-primary">Please enter a Number</h1>
+      );
+    }
+
+    let total = "";
+    const numberRightTriangle = [];
+    for (let i = 1; i <= input; i++) {
+      total = total + " " + i;
+      numberRightTriangle.push(total);
+    }
+
+    return numberRightTriangle.map((data, index) => (
+      <h1 className="col-12 w-100 text-primary" key={index}>
+        {data}
+      </h1>
+    ));
+  };
   return (
     <div className="container p-5">
       <div className="row">
@@ -9,9 +32,13 @@ export default function NumberRightTriangle() {
             type="text"
             className="w-100 lead"
             placeholder="Input"
+            value={input}
+            onChange={(e) => {
+              setInput(e.target.value);
+            }}
           />
         </div>
-        Hello World
+        {getNumberRightTriangle()}
       </div>
     </div>
   );
