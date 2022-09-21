1ba53b30c59b5d14f252d5267b8426ae0612aa3f
john-glay <johnglayc.bunao@gmail.com>
2022-09-21 14:05:02 +0800

    Right Triangle

--- src/pages/RightTriangle.jsx
+++ src/pages/RightTriangle.jsx
@@ -1,6 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function RightTriangle() {
+  const [input, setInput] = useState("");
+
+  const getRightTriangle = () => {
+    // Check if letter or null
+    if (isNaN(input) || !input) {
+      return (
+        <h1 className="col-12 w-100 text-primary">Please enter a Number</h1>
+      );
+    }
+
+    let total = "";
+    const triangle = [];
+    for (let i = 1; i <= input; i++) {
+      total = total.concat(" *");
+      triangle.push(total);
+    }
+
+    return triangle.map((data, index) => (
+      <h1 className="col-12 w-100 text-primary" key={index}>
+        {data}
+      </h1>
+    ));
+  };
+
   return (
     <div className="container p-5">
       <div className="row">
@@ -9,9 +33,13 @@ export default function RightTriangle() {
             type="text"
             className="w-100 lead"
             placeholder="Input"
+            value={input}
+            onChange={(e) => {
+              setInput(e.target.value);
+            }}
           />
         </div>
-        Hello World
+        {getRightTriangle()}
       </div>
     </div>
   );
