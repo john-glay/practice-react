729f32d4c9fbb069342585cea1ddd6d4608faf9e
john-glay <johnglayc.bunao@gmail.com>
2022-09-21 14:14:46 +0800

    Triangle

--- src/pages/Triangle.jsx
+++ src/pages/Triangle.jsx
@@ -1,6 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function Triangle() {
+  const [input, setInput] = useState("");
+
+  const getTriangle = () => {
+    // Check if letter or null
+    if (isNaN(input) || !input) {
+      return (
+        <h1 className="col-12 w-100 text-primary">Please enter a Number</h1>
+      );
+    }
+
+    let total = "";
+    const triangle = [];
+    for (let i = 1; i <= input; i++) {
+      total = total.concat(" *");
+      triangle.push(total);
+    }
+
+    const fullTriangle = [].concat(
+      triangle,
+      // We use slice to delete the part of an array
+      triangle.slice().reverse().slice(1)
+    );
+
+    return fullTriangle.map((data, index) => (
+      <h1 className="col-12 w-100 text-primary" key={index}>
+        {data}
+      </h1>
+    ));
+  };
+
   return (
     <div className="container p-5">
       <div className="row">
@@ -9,9 +39,13 @@ export default function Triangle() {
             type="text"
             className="w-100 lead"
             placeholder="Input"
+            value={input}
+            onChange={(e) => {
+              setInput(e.target.value);
+            }}
           />
         </div>
-        Hello World
+        {getTriangle()}
       </div>
     </div>
   );
