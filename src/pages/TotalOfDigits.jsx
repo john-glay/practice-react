e832a46315955b2d479c493dc7747b795bc7d5f7
john-glay <johnglayc.bunao@gmail.com>
2022-09-21 12:40:31 +0800

    Total of Digits

--- src/pages/TotalOfDigits.jsx
+++ src/pages/TotalOfDigits.jsx
@@ -1,6 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function TotalOfDigits() {
+  const [input, setInput] = useState("");
+
+  const getTotalOfDigits = () => {
+    let total = 0;
+
+    // Used forEach to perform specified action for each element in an array
+    input.split("").forEach((char) => {
+      if (!isNaN(char)) {
+        total = total + parseInt(char);
+      }
+    });
+
+    return total;
+  };
+
   return (
     <div className="container p-5">
       <div className="row">
@@ -9,9 +24,13 @@ export default function TotalOfDigits() {
             type="text"
             className="w-100 lead"
             placeholder="Input"
+            value={input}
+            onChange={(e) => {
+              setInput(e.target.value);
+            }}
           />
         </div>
-        <h1 className="col-12 w-100 text-primary">Hello Wrold</h1>
+        <h1 className="col-12 w-100 text-primary">{getTotalOfDigits()}</h1>
       </div>
     </div>
   );
