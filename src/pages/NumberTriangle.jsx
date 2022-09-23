b53735f0efdca513a453dc1291164540ba6637db
john-glay <johnglayc.bunao@gmail.com>
2022-09-23 13:41:49 +0800

    Final Commit

--- src/pages/NumberTriangle.jsx
+++ src/pages/NumberTriangle.jsx
@@ -1,6 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function NumberTriangle() {
+  const [input, setInput] = useState("");
+
+  const getNumberTriangle = () => {
+    // Check if letter or null
+    if (isNaN(input) || !input) {
+      return (
+        <h1 className="col-12 w-100 text-primary">Please enter a Number</h1>
+      );
+    }
+
+    let total = "";
+    const numberRightTriangle = [];
+    for (let i = 1; i <= input; i++) {
+      total = total + " " + i;
+      numberRightTriangle.push(total);
+    }
+
+    let fullNumberTriangle = [].concat(
+      numberRightTriangle,
+      numberRightTriangle.slice().reverse().slice(1)
+    );
+
+    return fullNumberTriangle.map((data, index) => (
+      <h1 className="col-12 w-100 text-primary" key={index}>
+        {data}
+      </h1>
+    ));
+  };
+
   return (
     <div className="container p-5">
       <div className="row">
@@ -9,9 +38,13 @@ export default function NumberTriangle() {
             type="text"
             className="w-100 lead"
             placeholder="Input"
+            value={input}
+            onChange={(e) => {
+              setInput(e.target.value);
+            }}
           />
         </div>
-            Hello World
+        {getNumberTriangle()}
       </div>
     </div>
   );
--- src/pages/Vowels.jsx
+++ src/pages/Vowels.jsx
@@ -1,6 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function Vowels() {
+  const [input, setInput] = useState("");
+
+  const getVowels = () => {
+    let total = 0;
+
+    // We used toLowerCase to convert all the alphabetic characters in a string to lowercase.
+    input.split("").forEach((char) => {
+      if (char.toLowerCase().match(/[aeiou]/)) {
+        total = total + 1;
+      }
+    });
+
+    return total;
+  };
   return (
     <div className="container p-5">
       <div className="row">
@@ -9,11 +23,13 @@ export default function Vowels() {
             type="text"
             className="w-100 lead"
             placeholder="Input"
+            value={input}
+            onChange={(e) => {
+              setInput(e.target.value);
+            }}
           />
         </div>
-        <h1 className="col-12 w-100 text-primary">
-          Hello World
-        </h1>
+        <h1 className="col-12 w-100 text-primary">{getVowels()}</h1>
       </div>
     </div>
   );
