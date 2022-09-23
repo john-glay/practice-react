994e3d0e4af5fe99db668b5aa5671c6cb27360db
john-glay <johnglayc.bunao@gmail.com>
2022-09-23 18:30:57 +0800

    Revert "Final Commit"
    This reverts commit b53735f0efdca513a453dc1291164540ba6637db.

--- src/pages/GetTheLargest.jsx
+++ src/pages/GetTheLargest.jsx
@@ -1,14 +1,6 @@
-import React, { useState } from "react";
+import React from "react";
 
 export default function GetTheLargest() {
-  const [firstInput, setFirstInput] = useState("");
-  const [secondInput, setSecondInput] = useState("");
-  const [thirdInput, setThirdInput] = useState("");
-
-  const getGetTheLargest = () => {
-    return Math.max(firstInput, secondInput, thirdInput);
-  }
-
   return (
     <div className="container p-5">
       <div className="row">
@@ -17,31 +9,19 @@ export default function GetTheLargest() {
             type="text"
             className="lead"
             placeholder="Input"
-            value={firstInput}
-            onChange={(e) => {
-              setFirstInput(e.target.value);
-            }}
           />
           <input
             type="text"
             className="lead"
             placeholder="Input"
-            value={secondInput}
-            onChange={(e) => {
-              setSecondInput(e.target.value);
-            }}
           />
           <input
             type="text"
             className="lead"
             placeholder="Input"
-            value={thirdInput}
-            onChange={(e) => {
-              setThirdInput(e.target.value);
-            }}
           />
         </div>
-        <h1 className="col-12 w-100 text-primary">{getGetTheLargest()}</h1>
+        <h1 className="col-12 w-100 text-primary">Hello World</h1>
       </div>
     </div>
   );
--- src/pages/Vowels.jsx
+++ src/pages/Vowels.jsx
@@ -1,20 +1,6 @@
-import React, { useState } from "react";
+import React from "react";
 
 export default function Vowels() {
-  const [input, setInput] = useState("");
-
-  const getVowels = () => {
-    let total = 0;
-
-    // We used toLowerCase to convert all the alphabetic characters in a string to lowercase.
-    input.split("").forEach((char) => {
-      if (char.toLowerCase().match(/[aeiou]/)) {
-        total = total + 1;
-      }
-    });
-
-    return total;
-  };
   return (
     <div className="container p-5">
       <div className="row">
@@ -23,13 +9,11 @@ export default function Vowels() {
             type="text"
             className="w-100 lead"
             placeholder="Input"
-            value={input}
-            onChange={(e) => {
-              setInput(e.target.value);
-            }}
           />
         </div>
-        <h1 className="col-12 w-100 text-primary">{getVowels()}</h1>
+        <h1 className="col-12 w-100 text-primary">
+          Hello World
+        </h1>
       </div>
     </div>
   );
